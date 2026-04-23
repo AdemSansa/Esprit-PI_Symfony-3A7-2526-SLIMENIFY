f89d95fc6950774f7ac7cfcff3e85a9ed25a8f8c
ahmeddegachi92 <ahmeddegachi92@gmail.com>
2026-04-23 07:10:09 +0100

    Update Notification.php

--- src/Entity/Notification.php
+++ src/Entity/Notification.php
@@ -6,11 +6,6 @@ use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'notifications')]
-use App\Repository\NotificationRepository;
-use Doctrine\ORM\Mapping as ORM;
-
-#[ORM\Entity(repositoryClass: NotificationRepository::class)]
-#[ORM\Table(name: 'notification')]
 class Notification
 {
     #[ORM\Id]
@@ -60,99 +55,4 @@ class Notification
     public function setIsRead(bool $isRead): static { $this->isRead = $isRead; return $this; }
     public function getEventId(): ?int { return $this->eventId; }
     public function setEventId(?int $eventId): static { $this->eventId = $eventId; return $this; }
-    #[ORM\Column(type: 'string', length: 255)]
-    private ?string $title = null;
-
-    #[ORM\Column(type: 'text')]
-    private ?string $message = null;
-
-    #[ORM\Column(type: 'boolean', options: ['default' => false])]
-    private bool $isRead = false;
-
-    #[ORM\Column(type: 'datetime')]
-    private ?\DateTimeInterface $createdAt = null;
-
-    #[ORM\ManyToOne(targetEntity: User::class)]
-    #[ORM\JoinColumn(name: 'user_id', referencedColumnName: 'id', onDelete: 'CASCADE')]
-    private ?User $user = null;
-
-    #[ORM\ManyToOne(targetEntity: Blog::class)]
-    #[ORM\JoinColumn(name: 'blog_id', referencedColumnName: 'id', onDelete: 'CASCADE')]
-    private ?Blog $blog = null;
-
-    public function __construct()
-    {
-        $this->createdAt = new \DateTime();
-    }
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
-
-    public function setTitle(string $title): self
-    {
-        $this->title = $title;
-        return $this;
-    }
-
-    public function getMessage(): ?string
-    {
-        return $this->message;
-    }
-
-    public function setMessage(string $message): self
-    {
-        $this->message = $message;
-        return $this;
-    }
-
-    public function isRead(): bool
-    {
-        return $this->isRead;
-    }
-
-    public function setIsRead(bool $isRead): self
-    {
-        $this->isRead = $isRead;
-        return $this;
-    }
-
-    public function getCreatedAt(): ?\DateTimeInterface
-    {
-        return $this->createdAt;
-    }
-
-    public function setCreatedAt(\DateTimeInterface $createdAt): self
-    {
-        $this->createdAt = $createdAt;
-        return $this;
-    }
-
-    public function getUser(): ?User
-    {
-        return $this->user;
-    }
-
-    public function setUser(?User $user): self
-    {
-        $this->user = $user;
-        return $this;
-    }
-
-    public function getBlog(): ?Blog
-    {
-        return $this->blog;
-    }
-
-    public function setBlog(?Blog $blog): self
-    {
-        $this->blog = $blog;
-        return $this;
-    }
 }
