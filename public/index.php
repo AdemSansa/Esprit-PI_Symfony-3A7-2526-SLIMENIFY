9fe729e05a9e4ff607df911909e53b052132e133
AdemSansa <sansaadem@rades.r-iset.tn>
2026-06-08 18:34:10 +0100

    Revert to php built-in server and fix static assets routing

--- public/index.php
+++ public/index.php
@@ -1,5 +1,15 @@
 <?php
 
+// Serve static files when using the built-in PHP web server (php -S)
+if (php_sapi_name() === 'cli-server') {
+    $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $path = __DIR__ . $url;
+    // If the file exists and is not a directory, let the built-in server handle it directly
+    if ($url !== '/' && file_exists($path) && is_file($path)) {
+        return false;
+    }
+}
+
 set_time_limit(30);
 
 use App\Kernel;
